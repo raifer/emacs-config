84f5e688e707dd329654728a17894638ea3e3839
Mathieu Barbe <mat.barbe@gmail.com>
2017-01-31 22:07:20 +0100

    nettoyage de add package et active gestion paquets au démarage

--- lisp/add-packages-archives.el
+++ lisp/add-packages-archives.el
@@ -3,22 +3,18 @@
 ; Gestion du sytème de package avec Emacs
 (require 'package)
 
- ;ajout du server org pour elpa
+;ajout du server melpa pour elpa
 (add-to-list 'package-archives
-	     '("org" . "http://orgmode.org/elpa/") t)
-
- ;ajout du server melpa pour elpa
-(add-to-list 'package-archives
-	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+	     '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
 					; Mise à jour auto de la liste des paquets
 ; La suite est commenté pour que Emacs démarre plus rapidement
 ; Refresh the packages descriptions
-;(unless package-archive-contents    
-;  (package-refresh-contents))
+(unless package-archive-contents    
+(package-refresh-contents))
 
 ; initialise le système de paquet
-;(package-initialize)                
+(package-initialize)
 ; List all of packages to load
 ;(setq package-load-list '(all))     
 
