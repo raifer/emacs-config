11c9fbb0939119b48ba4a4bd80f535f8c3333b92
Mathieu Barbe <mat.barbe@gmail.com>
2017-01-31 22:11:44 +0100

    nettoyage de speechd-el

--- lisp/speech.el
+++ lisp/speech.el
@@ -2,32 +2,34 @@
 
 ; speech dispacher 
 ; ajout du du dossier /usr/share/emacs/site-lisp/speechd-el au path
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/speechd-el")
+;(add-to-list 'load-path "/usr/share/emacs/site-lisp/speechd-el")
 
-; Charge speechd-speak
-;(load-library "speechd-speak")
+; Charge speechd-el
 (autoload 'speechd-speak "speechd-speak" nil t) 
 ; Lance speechd-el
 (speechd-speak)
 
 ; configuration de speechd-el
-(custom-set-variables
-;(load-library "speechd-ssip")
- '(speechd-out-active-drivers (quote (ssip)))
-
-; echo touche off
-'(speechd-speak-echo nil)
+;echo character word or nil
+(setq speechd-speak-echo 'word)
 
 ; Ne lit pas les commande tapées
-'(speechd-speak-read-command-keys nil)
+(setq speechd-speak-read-command-keys nil)
+
+
+
+(custom-set-variables
+;(load-library "speechd-ssip")
+;'(speechd-out-active-drivers (quote (ssip)))
 
 ; langue
-'(speechd-speak-input-method-languages (quote (("fr" . "") ("" . "") ("" . "fr"))))
+;'(speechd-speak-input-method-languages (quote (("fr" . "") ("" . "") ("" . "fr"))))
 
-					; Règle la vitesse
-'(speechd-voices (quote ((nil (rate . 40) (capital-character-mode . spell)))))
+					; Règle le débit
+'(speechd-voices (quote ((nil (rate . 15) (capital-character-mode . none)))))
 )
 
 					; shortkey
 ; lire la ligne en cours avec control l
 (global-set-key [(C l)] 'speechd-speak-read-line)
+
